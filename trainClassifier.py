748ab0ef7199b58665ae9115ab0da1ed3e638682
keshav <rkeshav205@gmail.com>
2026-01-04 16:51:04 +0530

    Update trainClassifier.py

--- trainClassifier.py
+++ trainClassifier.py
@@ -1,4 +1,4 @@
-import pickle
+import joblib
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -23,6 +23,6 @@ score = accuracy_score(y_predict, y_test)
 
 print('{}% of samples were classified correctly !'.format(score * 100))
 
-f = open('model.p', 'wb')
-pickle.dump({'model': model}, f)
-f.close()
+
+joblib.dump(model_dict, "model.joblib")
+
